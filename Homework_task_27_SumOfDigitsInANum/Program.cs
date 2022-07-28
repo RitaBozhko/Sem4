1f5ff7e877a23cae55b9d96982983afd6670ec8f
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-07-28 18:45:37 +0300

    Homework to Seminar 4

---
+++ Homework_task_27_SumOfDigitsInANum/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int userNum = int.Parse(Console.ReadLine()!);
+
+int DigitsSum (int userNum)
+{
+    int result = 0;
+    while(userNum != 0)
+    {
+        result += userNum%10;
+        userNum = userNum/10;
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма цифр числа {userNum} равна {DigitsSum(userNum)}");
---
+++ Homework_task_29_PrintArray/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: 
+// Напишите программу, которая задаёт массив из 8 элементов
+//  и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+int n = array.Length;
+//Console.WriteLine(n);
+
+int[] FillArray(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        array[i] = new Random().Next(1,100);
+    }
+    return array;
+}
+
+Console.Write("[{0}]", string.Join(", ", FillArray(n)));
